109cc350ee0977c733c9f6a686d1cf0ce3ce49a5
mk <psglgd8051@gmail.com>
2024-09-04 00:37:21 +0800

    删除无用代码

--- scripyX-server/src/main/java/com/mark/ScripyXServerApplication.java
+++ scripyX-server/src/main/java/com/mark/ScripyXServerApplication.java
@@ -1,10 +1,8 @@
 package com.mark;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAdminServer
 @SpringBootApplication
 public class ScripyXServerApplication {
     public static void main(String[] args) {
