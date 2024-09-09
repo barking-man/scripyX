689db1e90d740c95b155c50225261086bcb4695f
mk <psglgd8051@gmail.com>
2024-09-10 00:06:22 +0800

    spring boot admin初始化

--- scripyX-server/src/main/java/com/mark/controller/TestServerController.java
+++ scripyX-server/src/main/java/com/mark/controller/TestServerController.java
@@ -1,5 +1,6 @@
 package com.mark.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RequestMapping("/test/server")
 @RestController
+@Slf4j
 public class TestServerController {
     @Value("${TEST_VALUE}")
     private String TEST_VALUE;
 
     @RequestMapping(value = "/getTestValue", method = RequestMethod.GET)
     public String getTestValue() {
+        log.info("info级别日志");
+        log.debug("debug 日志级别");
         return TEST_VALUE;
     }
 }
